08a7a1372272303eee1070a5d76381a2da77c027
chengang7 <819240821@qq.com>
2023-05-03 22:27:27 +0800

    图片组件的使用

--- flutter_app02/lib/main.dart
+++ flutter_app02/lib/main.dart
@@ -9,13 +9,28 @@ void main() {
         body: Column(
           children: const [
             MyApp(),
-            MyButton(),
-            MyText(),
+            SizedBox(
+              height: 20,
+            ),
+            Circular(),
+            SizedBox(
+              height: 20,
+            ),
+            ClipImage(),
+            SizedBox(
+              height: 20,
+            ),
+            LocalImage(),
           ],
         )),
   ));
 }
 
+/*
+图片地址：
+https://c-ssl.dtstatic.com/uploads/item/202003/19/20200319182804_lrmkk.thumb.1000_0.jpg
+https://www.itying.com/themes/itying/images/ionic4.png
+*/
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -23,86 +38,74 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        margin: const EdgeInsets.fromLTRB(0, 60, 0, 0),
-        //配置container容器内元素的方位
-        alignment: Alignment.center,
-        width: 200,
-        height: 200,
-        //位移
-        //transform: Matrix4.translationValues(40, 0, 0),
-        //旋转
-        transform: Matrix4.rotationZ(0.02),
-        decoration: BoxDecoration(
-            //背景颜色
-            color: Colors.yellow,
-            //边框
-            border: Border.all(color: Colors.red, width: 2),
-            //配置圆角
-            borderRadius: BorderRadius.circular(10),
-            //配置阴影
-            boxShadow: const [BoxShadow(color: Colors.black, blurRadius: 20)],
-            //配置渐变(线性渐变)
-            gradient:
-                const LinearGradient(colors: [Colors.red, Colors.yellow])),
-
-        child: const Text(
-          "你好 flutter",
-          style: TextStyle(color: Colors.white),
+        margin: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+        width: 150,
+        height: 150,
+        decoration: const BoxDecoration(color: Colors.yellow),
+        child: Image.network(
+          "https://www.itying.com/themes/itying/images/ionic4.png",
+          //对图片进行缩放
+          //scale: 2,
+          //对图片进行剪切
+          //fit: BoxFit.cover,
+          //对图片进行平铺
+          repeat: ImageRepeat.repeatX,
         ),
       ),
     );
   }
 }
 
-class MyButton extends StatelessWidget {
-  const MyButton({Key? key}) : super(key: key);
+//实现一个圆形图片
+class Circular extends StatelessWidget {
+  const Circular({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      alignment: Alignment.center,
-      width: 200,
-      height: 40,
-      //EdgeInsets.fromLTRB 指定左上右下的边距
-      //EdgeInsets.all() 指定四周的距离
-      margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+      width: 150,
+      height: 150,
       decoration: BoxDecoration(
-          color: Colors.blue, borderRadius: BorderRadius.circular(10)),
-      child: const Text(
-        "按钮",
-        style: TextStyle(color: Colors.white, fontSize: 20),
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(75),
+          image: const DecorationImage(
+              image: NetworkImage(
+                  "https://www.itying.com/themes/itying/images/ionic4.png"),
+              fit: BoxFit.cover)),
+    );
+  }
+}
+
+//实现一个圆形图片2
+class ClipImage extends StatelessWidget {
+  const ClipImage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipOval(
+      child: Image.network(
+        "https://www.itying.com/themes/itying/images/ionic4.png",
+        width: 150,
+        height: 150,
+        fit: BoxFit.cover,
       ),
     );
   }
 }
 
-class MyText extends StatelessWidget {
-  const MyText({Key? key}) : super(key: key);
+//加载本地图片
+class LocalImage extends StatelessWidget {
+  const LocalImage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 200,
-      height: 200,
-      margin: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+      width: 150,
+      height: 150,
       decoration: const BoxDecoration(color: Colors.yellow),
-      child: const Text(
-        "你好 我是flutter",
-        textAlign: TextAlign.center,
-        //最大显示行数
-        maxLines: 1,
-        //溢出显示几个点
-        overflow: TextOverflow.ellipsis,
-        style: TextStyle(
-            fontSize: 40,
-            //加粗900
-            fontWeight: FontWeight.w900,
-            color: Colors.red,
-            fontStyle: FontStyle.italic,
-            letterSpacing: 2,
-            decoration: TextDecoration.underline,
-            decorationColor: Colors.black,
-            decorationStyle: TextDecorationStyle.dashed),
+      child: Image.asset(
+        "images/a.jpeg",
+        fit: BoxFit.cover,
       ),
     );
   }
