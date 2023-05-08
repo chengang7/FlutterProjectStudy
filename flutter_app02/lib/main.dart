ded57ae376691da29d49c004081cb63f1febf457
chengang7 <819240821@qq.com>
2023-05-08 11:14:50 +0800

    flutter listView使用方式二

--- flutter_app02/lib/main.dart
+++ flutter_app02/lib/main.dart
@@ -18,49 +18,40 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: const <Widget>[
+      padding: const EdgeInsets.all(10),
+      children: <Widget>[
         ListTile(
-          leading: Icon(Icons.home),
-          title: Text("首页"),
-          trailing: Icon(Icons.chevron_right),
+          leading: Image.network(
+              "https://pics7.baidu.com/feed/d439b6003af33a87aad30f65786532345243b5dc.jpeg@f_auto?token=0ba4495255f1f561fc8af2e9422e3d8c"),
+          title: const Text("多地用电量整体呈现出积极信号"),
+          subtitle: const Text(
+              "据中央广播电视总台中国之声《新闻和报纸摘要》报道，电力数据是反映经济运行的“晴雨表”和“风向标”。今年以来，随着消费信心逐步回暖、企业复工开足马力生产，多地用电量整体呈现出积极信号，经济增长企稳回升态势明显。"),
         ),
-        Divider(),
+        const Divider(),
         ListTile(
-          leading: Icon(
-            Icons.assignment,
-            color: Colors.red,
-          ),
-          title: Text("全部订单"),
-          trailing: Icon(Icons.chevron_right),
+          leading: Image.network(
+              "https://pics7.baidu.com/feed/d439b6003af33a87aad30f65786532345243b5dc.jpeg@f_auto?token=0ba4495255f1f561fc8af2e9422e3d8c"),
+          title: const Text("多地用电量整体呈现出积极信号"),
+          subtitle: const Text(
+              "据中央广播电视总台中国之声《新闻和报纸摘要》报道，电力数据是反映经济运行的“晴雨表”和“风向标”。今年以来，随着消费信心逐步回暖、企业复工开足马力生产，多地用电量整体呈现出积极信号，经济增长企稳回升态势明显。"),
         ),
-        Divider(),
+        const Divider(),
         ListTile(
-          leading: Icon(
-            Icons.payment,
-            color: Colors.green,
-          ),
-          title: Text("待付款"),
-          trailing: Icon(Icons.chevron_right),
+          leading: Image.network(
+              "https://pics7.baidu.com/feed/d439b6003af33a87aad30f65786532345243b5dc.jpeg@f_auto?token=0ba4495255f1f561fc8af2e9422e3d8c"),
+          title: const Text("多地用电量整体呈现出积极信号"),
+          subtitle: const Text(
+              "据中央广播电视总台中国之声《新闻和报纸摘要》报道，电力数据是反映经济运行的“晴雨表”和“风向标”。今年以来，随着消费信心逐步回暖、企业复工开足马力生产，多地用电量整体呈现出积极信号，经济增长企稳回升态势明显。"),
         ),
-        Divider(),
+        const Divider(),
         ListTile(
-          leading: Icon(
-            Icons.favorite,
-            color: Colors.lightGreen,
-          ),
-          title: Text("我的收藏"),
-          trailing: Icon(Icons.chevron_right),
+          title: const Text("多地用电量整体呈现出积极信号"),
+          subtitle: const Text(
+              "据中央广播电视总台中国之声《新闻和报纸摘要》报道，电力数据是反映经济运行的“晴雨表”和“风向标”。今年以来，随着消费信心逐步回暖、企业复工开足马力生产，多地用电量整体呈现出积极信号，经济增长企稳回升态势明显。"),
+          trailing: Image.network(
+              "https://pics7.baidu.com/feed/d439b6003af33a87aad30f65786532345243b5dc.jpeg@f_auto?token=0ba4495255f1f561fc8af2e9422e3d8c"),
         ),
-        Divider(),
-        ListTile(
-          leading: Icon(
-            Icons.people,
-            color: Colors.black54,
-          ),
-          title: Text("在线客服"),
-          trailing: Icon(Icons.chevron_right),
-        ),
-        Divider(),
+        const Divider(),
       ],
     );
   }
