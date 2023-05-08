096197b5f544b6cb446765ebe852b84f33efbb58
chengang7 <819240821@qq.com>
2023-05-08 11:30:51 +0800

    flutter listView使用方式三 图文列表的使用

--- flutter_app02/lib/main.dart
+++ flutter_app02/lib/main.dart
@@ -20,38 +20,66 @@ class MyHomePage extends StatelessWidget {
     return ListView(
       padding: const EdgeInsets.all(10),
       children: <Widget>[
-        ListTile(
-          leading: Image.network(
-              "https://pics7.baidu.com/feed/d439b6003af33a87aad30f65786532345243b5dc.jpeg@f_auto?token=0ba4495255f1f561fc8af2e9422e3d8c"),
-          title: const Text("多地用电量整体呈现出积极信号"),
-          subtitle: const Text(
-              "据中央广播电视总台中国之声《新闻和报纸摘要》报道，电力数据是反映经济运行的“晴雨表”和“风向标”。今年以来，随着消费信心逐步回暖、企业复工开足马力生产，多地用电量整体呈现出积极信号，经济增长企稳回升态势明显。"),
+        Image.network("https://www.itying.com/images/flutter/1.png"),
+        Container(
+          padding: const EdgeInsets.fromLTRB(0, 6, 0, 0),
+          height: 44,
+          child: const Text(
+            "我是一个文本",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 22),
+          ),
         ),
-        const Divider(),
-        ListTile(
-          leading: Image.network(
-              "https://pics7.baidu.com/feed/d439b6003af33a87aad30f65786532345243b5dc.jpeg@f_auto?token=0ba4495255f1f561fc8af2e9422e3d8c"),
-          title: const Text("多地用电量整体呈现出积极信号"),
-          subtitle: const Text(
-              "据中央广播电视总台中国之声《新闻和报纸摘要》报道，电力数据是反映经济运行的“晴雨表”和“风向标”。今年以来，随着消费信心逐步回暖、企业复工开足马力生产，多地用电量整体呈现出积极信号，经济增长企稳回升态势明显。"),
+        Image.network("https://www.itying.com/images/flutter/2.png"),
+        Container(
+          padding: const EdgeInsets.fromLTRB(0, 6, 0, 0),
+          height: 44,
+          child: const Text(
+            "我是一个文本",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 22),
+          ),
         ),
-        const Divider(),
-        ListTile(
-          leading: Image.network(
-              "https://pics7.baidu.com/feed/d439b6003af33a87aad30f65786532345243b5dc.jpeg@f_auto?token=0ba4495255f1f561fc8af2e9422e3d8c"),
-          title: const Text("多地用电量整体呈现出积极信号"),
-          subtitle: const Text(
-              "据中央广播电视总台中国之声《新闻和报纸摘要》报道，电力数据是反映经济运行的“晴雨表”和“风向标”。今年以来，随着消费信心逐步回暖、企业复工开足马力生产，多地用电量整体呈现出积极信号，经济增长企稳回升态势明显。"),
+        Image.network("https://www.itying.com/images/flutter/3.png"),
+        Container(
+          padding: const EdgeInsets.fromLTRB(0, 6, 0, 0),
+          height: 44,
+          child: const Text(
+            "我是一个文本",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 22),
+          ),
         ),
-        const Divider(),
-        ListTile(
-          title: const Text("多地用电量整体呈现出积极信号"),
-          subtitle: const Text(
-              "据中央广播电视总台中国之声《新闻和报纸摘要》报道，电力数据是反映经济运行的“晴雨表”和“风向标”。今年以来，随着消费信心逐步回暖、企业复工开足马力生产，多地用电量整体呈现出积极信号，经济增长企稳回升态势明显。"),
-          trailing: Image.network(
-              "https://pics7.baidu.com/feed/d439b6003af33a87aad30f65786532345243b5dc.jpeg@f_auto?token=0ba4495255f1f561fc8af2e9422e3d8c"),
+        Image.network("https://www.itying.com/images/flutter/4.png"),
+        Container(
+          padding: const EdgeInsets.fromLTRB(0, 6, 0, 0),
+          height: 44,
+          child: const Text(
+            "我是一个文本",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 22),
+          ),
         ),
-        const Divider(),
+        Image.network("https://www.itying.com/images/flutter/5.png"),
+        Container(
+          padding: const EdgeInsets.fromLTRB(0, 6, 0, 0),
+          height: 44,
+          child: const Text(
+            "我是一个文本",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 22),
+          ),
+        ),
+        Image.network("https://www.itying.com/images/flutter/6.png"),
+        Container(
+          padding: const EdgeInsets.fromLTRB(0, 6, 0, 0),
+          height: 44,
+          child: const Text(
+            "我是一个文本",
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 22),
+          ),
+        ),
       ],
     );
   }
