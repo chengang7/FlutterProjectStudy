3fb19ebc846c5630b48b866a7831706c086ada02
chengang7 <819240821@qq.com>
2023-05-09 16:20:01 +0800

    相关组件的使用

---
+++ flutter_app02/lib/main14_Card组件的使用2.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app02/fonts/iconfont.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(primarySwatch: Colors.blue),
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter App"),
+      ),
+      body: const HomePage(),
+    ),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Card(
+          margin: const EdgeInsets.all(10),
+          elevation: 10,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          child: Column(
+            children: [
+              AspectRatio(
+                  aspectRatio: 16 / 9,
+                  child: Image.network(
+                    "https://www.itying.com/images/flutter/1.png",
+                    fit: BoxFit.cover,
+                  )),
+              ListTile(
+                leading: ClipOval(
+                  child: Image.network(
+                    "https://www.itying.com/images/flutter/1.png",
+                    fit: BoxFit.cover,
+                    height: 40,
+                    width: 40,
+                  ),
+                ),
+                title: const Text("xxxx"),
+                subtitle: const Text("xxxxxxxx"),
+              ),
+            ],
+          ),
+        ),
+        Card(
+          margin: const EdgeInsets.all(10),
+          elevation: 10,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          child: Column(
+            children: [
+              AspectRatio(
+                  aspectRatio: 16 / 9,
+                  child: Image.network(
+                    "https://www.itying.com/images/flutter/1.png",
+                    fit: BoxFit.cover,
+                  )),
+              const ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://www.itying.com/images/flutter/1.png"),
+                ),
+                title: Text("xxxx"),
+                subtitle: Text("xxxxxxxx"),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
