ac8933c409217cfd4d100ecd1d626e97e3deb602
chengang7 <819240821@qq.com>
2023-05-16 16:09:38 +0800

    AppBar的相关使用

--- flutter_app02/lib/main.dart
+++ flutter_app02/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import './pages/tabs.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +10,92 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //去掉debug图标
+      debugShowCheckedModeBanner: false,
       title: "Flutter Demo",
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const Tabs(),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {
+            print("左侧的按钮图标");
+          },
+        ),
+        title: const Text("Flutter App"),
+        backgroundColor: Colors.red,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              print("搜索图标");
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.more_horiz),
+            onPressed: () {
+              print("更多图标");
+            },
+          ),
+        ],
+        bottom: TabBar(controller: _tabController, tabs: const [
+          Tab(
+            child: Text("关注"),
+          ),
+          Tab(child: Text("热门")),
+          Tab(child: Text("视频"))
+        ]),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          ListView(
+            children: const [
+              ListTile(
+                title: Text("我是关注列表"),
+              )
+            ],
+          ),
+          ListView(
+            children: const [
+              ListTile(
+                title: Text("我是热门列表"),
+              )
+            ],
+          ),
+          ListView(
+            children: const [
+              ListTile(
+                title: Text("我是视频列表"),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
