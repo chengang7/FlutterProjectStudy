e3650328faa7d36c41e1480b94ca7438557afa78
chengang7 <819240821@qq.com>
2023-05-16 14:36:15 +0800

    11

--- flutter_app02/lib/pages/tabs.dart
+++ flutter_app02/lib/pages/tabs.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app02/pages/tabs/message.dart';
+import 'package:flutter_app02/pages/tabs/user.dart';
 import './tabs/home.dart';
 import './tabs/category.dart';
 import './tabs/setting.dart';
@@ -12,7 +14,13 @@ class Tabs extends StatefulWidget {
 
 class _TabsState extends State<Tabs> {
   int _currentIndex = 0;
-  final List<Widget> _pages = const [HomePage(), CategoryPage(), SettingPage()];
+  final List<Widget> _pages = const [
+    HomePage(),
+    CategoryPage(),
+    MessagePage(),
+    SettingPage(),
+    UserPage()
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -23,14 +31,34 @@ class _TabsState extends State<Tabs> {
       body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
           currentIndex: _currentIndex,
+          type: BottomNavigationBarType.fixed,
           onTap: (value) => setState(() {
                 _currentIndex = value;
               }),
           items: const [
             BottomNavigationBarItem(icon: Icon(Icons.home), label: "首页"),
             BottomNavigationBarItem(icon: Icon(Icons.category), label: "分类"),
+            BottomNavigationBarItem(icon: Icon(Icons.message), label: "消息"),
             BottomNavigationBarItem(icon: Icon(Icons.settings), label: "设置"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "用户"),
           ]),
+      floatingActionButton: Container(
+        height: 50,
+        width: 50,
+        padding: const EdgeInsets.all(5),
+        margin: const EdgeInsets.only(top: 10),
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(25)),
+        child: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            setState(() {
+              _currentIndex = 2;
+            });
+          },
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
